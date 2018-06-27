51c65c1ea5aa20479a31ae6533c77cb109a67079
DikshaSaha <39985388+DikshaSaha@users.noreply.github.com>
2018-06-27 21:04:04 +0530

    Delete UserRepository.java

--- zomatoplus/repo/UserRepository.java
+++
@@ -1,9 +0,0 @@
-package com.thinkxfactor.zomatoplus.repo;
-
-import org.springframework.data.jpa.repository.JpaRepository;
-
-import com.thinkxfactor.zomatoplus.models.User;
-
-public interface UserRepository extends JpaRepository<User, Long>{
-
-}
